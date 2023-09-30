773151e02be34fb185184b164b87b53333b001db
David Sangrey <rixxan@hullseals.space>
2023-09-30 12:00:35 -0400

    Add Basic Password Awareness

--- tbo_armatracker/__main__.py
+++ tbo_armatracker/__main__.py
@@ -7,6 +7,7 @@ See license.md
 import asyncio
 import datetime
 import configparser
+import json
 import discord
 from discord.ext import commands
 import a2s
@@ -20,11 +21,16 @@ BOT_TOKEN = config["Discord"]["key"]
 SERVER_IP = config["ARMA"]["server_ip"]
 SERVER_PORT = config["ARMA"]["server_port"]
 MAINS_ID = config["Discord"]["channel"]
+THUMBNAIL_URL = (
+    "https://static.wixstatic.com/media/5474d8_6521e7baef3545a8a8c91b8a55726f7d~mv2.png"
+    "v1/fill/w_98,h_86,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/TBOA3_logo_1.png"
+)
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=">", intents=intents)
+
 
 async def get_server_info():
     """Query the ARMA Server for current status"""
@@ -38,6 +44,7 @@ async def get_server_info():
 async def on_ready():
     """Run these actions when the bot is fully online"""
     print("Trying purge + setup")
+    creds = await load_json()
     channel = discord.Client.get_channel(bot, int(MAINS_ID))
     bootup = await channel.send("Booting the Bot")
     await channel.purge()
@@ -45,7 +52,7 @@ async def on_ready():
     await info_mesages(ctx)
     await channel.send(embed=await format_gtx_embed())
     await channel.send(embed=await format_ts_embed())
-    await create_check(ctx)
+    await create_check(ctx, creds)
 
 
 @bot.command()
@@ -63,7 +70,7 @@ async def info_mesages(ctx):
     await ctx.send(logo)
 
 
-async def format_server_embeds(tbo):
+async def format_server_embeds(tbo, creds):
     """Format the embed for the server status"""
     # Create server info embed
     server_embed = discord.Embed(
@@ -72,15 +79,18 @@ async def format_server_embeds(tbo):
         description="Current Online Server",
         color=0x80FFFF,
     )
-    server_embed.set_thumbnail(
-        url="https://static.wixstatic.com/media/5474d8_6521e7baef3545a8a8c91b8a55726f7d~mv2.png/"
-        "v1/fill/w_98,h_86,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/TBOA3_logo_1.png"
-    )
+    server_embed.set_thumbnail(url=THUMBNAIL_URL)
     server_embed.add_field(name="Current Server", value=tbo.server_name, inline=False)
     if tbo.map_name:
         server_embed.add_field(name="Map", value=tbo.map_name, inline=False)
     server_embed.add_field(name="Address", value=SERVER_IP)
     server_embed.add_field(name="Port", value=tbo.port)
+
+    creds_dict = {pair["name"]: pair["pwd"] for pair in creds}
+    password = creds_dict.get(tbo.server_name)
+    if password:
+        server_embed.add_field(name="Password", value=password)
+
     return server_embed
 
 
@@ -93,10 +103,7 @@ async def format_player_embeds(players):
         description="Current Players Online",
         color=0x80FFFF,
     )
-    player_embed.set_thumbnail(
-        url="https://static.wixstatic.com/media/5474d8_6521e7baef3545a8a8c91b8a55726f7d~mv2.png/"
-        "v1/fill/w_98,h_86,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/TBOA3_logo_1.png"
-    )
+    player_embed.set_thumbnail(url=THUMBNAIL_URL)
 
     if players:
         for player in players:
@@ -137,7 +144,7 @@ async def format_ts_embed():
     )
     ts_embed.set_thumbnail(
         url="https://discourse-forums-images.s3.dualstack.us-east-2.amazonaws.com/"
-            "original/2X/2/269d8bb30efc4bdf5c99f1f27c2aeadc1ca2fa5d.png"
+        "original/2X/2/269d8bb30efc4bdf5c99f1f27c2aeadc1ca2fa5d.png"
     )
     ts_embed.add_field(name="Address", value="ts3l.gtxgaming.co.uk:10178")
     ts_embed.add_field(name="Password", value="tbo")
@@ -145,11 +152,11 @@ async def format_ts_embed():
 
 
 @bot.command()
-async def create_check(ctx):
+async def create_check(ctx, creds):
     """Begin the looping check!"""
     tbo, players = await get_server_info()
 
-    server_embed = await format_server_embeds(tbo)
+    server_embed = await format_server_embeds(tbo, creds)
     checkmsg1 = await ctx.send(embed=server_embed)
 
     player_embed = await format_player_embeds(players)
@@ -159,11 +166,18 @@ async def create_check(ctx):
         await asyncio.sleep(5)
         tbo, players = await get_server_info()
 
-        server_embed = await format_server_embeds(tbo)
+        server_embed = await format_server_embeds(tbo, creds)
         await checkmsg1.edit(embed=server_embed)
 
         player_embed = await format_player_embeds(players)
         await checkmsg2.edit(embed=player_embed)
+
+
+async def load_json():
+    """Load the latest server update file"""
+    with open("server.json", encoding="UTF-8") as jfile:
+        servers = json.load(jfile)
+        return servers
 
 
 if __name__ == "__main__":
